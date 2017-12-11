711426c4b6e41854d747c027370e62d48f3b8d72
unknown <v.a.stekolschikov@gmail.com>
2017-12-11 22:10:51 +0200

    v2.5

--- script.js
+++ script.js
@@ -15,23 +15,17 @@ VK.init({
 function authInfo(response) {    
     if(response.status=='connected') {  // если пользователь залогинен в ВК     
         if (typeof(response.session.user) == 'undefined') { // этого поля нет 
-            //тогда, когда пользователь был залогинен ранее
             VK.Api.call('users.get', { uid: response.session.mid }, function(r) { 
-                $('#VKInfo').html(r.response[0].first_name+' '+r.response[0].last_name); 
-                $('#VKInfo').append('user_id: '+response.session.mid); });
+                console.log(r.response[0].first_name+' '+r.response[0].last_name)
+                console.log('user_id: '+response.session.mid)
+            });
         } else {
-            // если авторизация прошла только что (от VK.Auth.login(authInfo);), то имя и фамилия уже будут в ответе   
-            $('#VKInfo').html(response.session.user.first_name+' '+response.session.user.last_name);
-            $('#VKInfo').append('user_id: '+response.session.mid);
+            console.log(response.session.user.first_name+' '+response.session.user.last_name);
+            console.log('user_id: '+response.session.mid);
         }
-
     } else {
         VK.Auth.login(authInfo); // опционально можем спалиться и вызвать всплывающее окно авторизации
     }
 }
 
-
 VK.Auth.getLoginStatus(authInfo) 
-
-
-
