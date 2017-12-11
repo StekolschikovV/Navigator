671feb8ecb82b1e989fc3da0aea36d0cb760a0ba
unknown <v.a.stekolschikov@gmail.com>
2017-12-11 22:19:38 +0200

    v3.1

--- src/scripts/vk.js
+++ src/scripts/vk.js
@@ -8,25 +8,56 @@
 //     }
 // });
 
+
+
+
+
+
+
+
+
+
+// VK.init({
+//     apiId: 6294974
+// });
+
+// function authInfo(response) {    
+//     if(response.status=='connected') {  // если пользователь залогинен в ВК     
+//         if (typeof(response.session.user) == 'undefined') { // этого поля нет 
+//             VK.Api.call('users.get', { uid: response.session.mid }, function(r) { 
+//                 console.log(r.response[0].first_name+' '+r.response[0].last_name)
+//                 console.log('user_id: '+response.session.mid)
+//             });
+//         } else {
+//             console.log(response.session.user.first_name+' '+response.session.user.last_name);
+//             console.log('user_id: '+response.session.mid);
+//         }
+//     } else {
+//         VK.Auth.login(authInfo); // опционально можем спалиться и вызвать всплывающее окно авторизации
+//     }
+// }
+
+// VK.Auth.getLoginStatus(authInfo) 
+
+
+
+
+
+
+
+
+
+
+
+
 VK.init({
     apiId: 6294974
 });
-
-function authInfo(response) {    
-    if(response.status=='connected') {  // если пользователь залогинен в ВК     
-        if (typeof(response.session.user) == 'undefined') { // этого поля нет 
-            VK.Api.call('users.get', { uid: response.session.mid }, function(r) { 
-                console.log(r.response[0].first_name+' '+r.response[0].last_name)
-                console.log('user_id: '+response.session.mid)
-            });
-        } else {
-            console.log(response.session.user.first_name+' '+response.session.user.last_name);
-            console.log('user_id: '+response.session.mid);
-        }
+function authInfo(response) {
+    if (response.session) {
+        alert('user: '+response.session.mid);
     } else {
-        VK.Auth.login(authInfo); // опционально можем спалиться и вызвать всплывающее окно авторизации
+        alert('not auth');
     }
 }
-
-VK.Auth.getLoginStatus(authInfo) 
 
