77701573557d76c2523676f78b8f36f5cc16e706
unknown <v.a.stekolschikov@gmail.com>
2017-12-11 22:01:39 +0200

    v2

--- src/scripts/vk.js
+++ src/scripts/vk.js
@@ -1,13 +1,34 @@
+// VK.init({
+//     apiId: 6294974,  // app id
+// });
+
+// VK.Auth.getLoginStatus(function (response) {
+//     if (response.status == 'connected') {
+//         console.log(response.status)
+//     }
+// });
+
 VK.init({
-    apiId: 6294974,  // app id
+    apiId: 6294974
 });
 
-VK.Auth.getLoginStatus(function (response) {
-    if (response.status == 'connected') {
-        console.log(response.status)
+function authInfo(response) {    
+    if(response.status=='connected') {  // если пользователь залогинен в ВК     
+        if (typeof(response.session.user) == 'undefined') { // этого поля нет 
+            //тогда, когда пользователь был залогинен ранее
+            VK.Api.call('users.get', { uid: response.session.mid }, function(r) { 
+                $('#VKInfo').html(r.response[0].first_name+' '+r.response[0].last_name); 
+                $('#VKInfo').append('user_id: '+response.session.mid); });
+        } else {
+            // если авторизация прошла только что (от VK.Auth.login(authInfo);), то имя и фамилия уже будут в ответе   
+            $('#VKInfo').html(response.session.user.first_name+' '+response.session.user.last_name);
+            $('#VKInfo').append(.'user_id: '+response.session.mid);
+        }
+
+    } else {
+        VK.Auth.login(authInfo); // опционально можем спалиться и вызвать всплывающее окно авторизации
     }
-});
+}
 
-
-
+$(function() { VK.Auth.getLoginStatus(authInfo); });
 
